e10ec5110ec72f26cfb68ba66feff2d48613173d
imranhosen <hossain3704@diu.edu.bd>
2020-02-08 11:37:24 +0600

    Again Change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel Update
+                    Laravel Update Again
                 </div>
 
                 <div class="links">
